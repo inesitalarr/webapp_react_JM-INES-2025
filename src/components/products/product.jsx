afeddee4bb5ffb41814c8ee06d13d7496f7177ba
JosemaArmen <josemari.armen@gmail.com>
2025-02-28 12:11:06 +0100

    Added products and GET petition

---
+++ src/components/products/product.jsx
@@ -0,0 +1,18 @@
+import './product.css';
+import {Image} from 'react-bootstrap';
+
+function Product(props) {
+    return (
+        <div className='producto'>
+            <div className='producto__descripcion'>
+                <Image src={`/imgs/products/${props.producto.imagen}`} roundedCircle width='100' />
+                <div className='producto__categoria'>{props.producto.categoría}</div>
+                <h2>Nombre: {props.producto.nombre}</h2>
+                <div className='producto__precio'>{props.producto.precio}</div>
+                <div className='producto__stock'>{props.producto.stock}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Product;
