61ab16742ae6048aa87df150f03d11827e170c6f
Ines Larrayoz <ilarrayozarrigote@gmail.com>
2025-02-28 11:40:04 +0100

    Add Footer component

---
+++ src/components/UI/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p>🌿 Comprometidos con el medio ambiente 🌎</p>
+        </footer>
+    );
+}
+
+export default Footer;
