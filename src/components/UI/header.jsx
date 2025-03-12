32e6f42cf1ca562c9470d9775ace06bdfdb1a5c6
Ines Larrayoz <ilarrayozarrigote@gmail.com>
2025-03-12 16:44:38 +0100

    Add cart item count badge to header component

--- src/components/UI/header.jsx
+++ src/components/UI/header.jsx
@@ -1,12 +1,31 @@
 import './header.css';
-import { Nav } from 'react-bootstrap';
+import { Nav, Badge } from 'react-bootstrap';
 import { Link } from 'react-router'; // Cuidado, usa 'react-router-dom' no 'react-router'
-import { useContext } from 'react';
+import { useContext, useState, useEffect} from 'react';
 import GlobalContext from '../../store/globalContext';
+import CarritoContext from '../../store/carritoContext';
 
 function Header(props) {
 
     const login = useContext(GlobalContext).login;
+    const cartItems = useContext(CarritoContext).listaProductos;
+    const [numItems, setNumItems] = useState(0);
+
+    useEffect(() => {
+      
+    
+        if (cartItems.length == 0) {
+            setNumItems(0);
+        } else {
+            setNumItems(cartItems.reduce((acc, item) => acc + item[1],0));
+        }
+
+        console.log(numItems);
+
+    }, [cartItems]);
+
+
+ 
 
     let parteLogin, parteLink;
     if (login) {
@@ -35,9 +54,17 @@ function Header(props) {
                     </Nav.Item>
                     <Nav.Item>
                         <Link to='/carrito'>
-                        <div className={`cart-icon ${props.isCartHighlighted ? 'highlighted' : ''} ${props.isCartHighlightedRed ? 'highlighted-red' : ''}`}>
-                            <img src='/imgs/icons/carrito.png' alt='Carrito' width='70' />
-                        </div>
+                            <div className={`cart-icon ${props.isCartHighlighted ? 'highlighted' : ''} ${props.isCartHighlightedRed ? 'highlighted-red' : ''}`}>
+                                {numItems > 0 ?
+                                    <span class="position-absolute top-20 start-99 translate-middle badge rounded-pill bg-danger" >
+                                        {numItems}
+                                        < span class="visually-hidden" > unread messages</span>
+                                    </span>
+                                    : null}
+
+                                <img src='/imgs/icons/carrito.png' alt='Carrito' width='70' />
+
+                            </div>
                         </Link>
                     </Nav.Item>
                     <Nav.Item>
@@ -46,10 +73,10 @@ function Header(props) {
                         </Link>
                     </Nav.Item>
                 </Nav>
-            </div>
-            
-        </header>
-       
+            </div >
+
+        </header >
+
     );
 }
 
